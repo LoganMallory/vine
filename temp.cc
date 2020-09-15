060b0dd797094c9ed4684262ae55e04c9a00848c
LoganMallory <logan.mallory@gmail.com>
2020-09-15 12:01:33 -0500

    ignore spotlight files

---
+++ temp.cc
@@ -0,0 +1,39 @@
+int OPT_LVL = 0;
+int DEBUG   = 1;
+#include "Vine.h"
+
+Vine<int> make_vine() {
+  Vine<int> vec(5, 10); //value 5, length 10
+  printf("vec.length = %u\nvec.values[0] = %d\n", vec.length, vec.values[0]);
+  return vec;
+}
+
+void make_edit(Vine<int>& vec) {
+  vec[0] = 91;
+}
+
+int main() {
+  Vine<int> myvine = make_vine();
+  printf("myvine.values[0] = %d\n", myvine.values[0]);
+  make_edit(myvine);
+  printf("myvine.values[0] = %d\n", myvine.values[0]);
+  return 0;
+}
+/*
+A common flow is:
+  Vine<int> vec2 = vec1 + v;
+  vec1+v invokes operator+
+  operator+ creates new Vine newvec*, returns dereferenced obejct (*newvec)
+  conversion constructor for vec2 is called (with newvec as arg)
+  values from newvec are copied into vec2.values
+
+  the question: is newvec and newvec.values ever deleted (and memory freed)?
+  if not, then vine conversion constructor would have to consume and destroy vine arg
+
+TODO: Vine<int> vec2 = vec1   should become    Vine<int> vec2 = vec1[];                                                 //Vine(const Vine<dtype>& vec)
+
+TODO: vec2 = vec1 + 9         should become    now consume newvec (point to its values, set its values pointer to null) //operator=
+TODO: make Vine(unsigned int/int) explicit so that Vine<int> vec = 5 is a compiler error
+
+TODO: it is illegal to use realloc on an array created by new
+*/
---
+++ test.cc
@@ -0,0 +1,44 @@
+const int OPT_LVL    = 0;
+const int TEST_DEBUG = 1;
+#include "Vine.h"
+#include <assert.h>
+#include "tests/constructor.cc"  //done
+#include "tests/destructor.cc"   //done
+#include "tests/assignment.cc"   //done
+#include "tests/indexing.cc"     //done
+#include "tests/equality.cc"     //done
+#include "tests/greaterthan.cc"  //done
+#include "tests/lessthan.cc"     //done
+#include "tests/addition.cc"     //done
+#include "tests/subtraction.cc"
+#include "tests/multiplication.cc"
+#include "tests/division.cc"
+
+//TODO: check that address of this->values changes when new object is returned in some operators
+//TODO: assert that errors are thrown when doing math on two Vines of different dimensions
+
+//handle overflow -- only called if compiled using -ftrapv
+void overflow_handler(int sig) {
+  printf("Overflow detected\n");
+  //abort the entire program
+  abort();
+}
+int main(){
+  signal(SIGILL, &overflow_handler);
+  unsigned int size = 1000; //size >= 2
+
+  test_constructor(size);
+  test_destructor(size);
+  test_assignment(size);
+  test_indexing(size);
+  test_greater_than(size);
+  test_less_than(size);
+  test_addition(size);
+  test_subtraction(size);
+  test_multiplication(size);
+  test_division(size);
+  test_equality(size);
+
+
+  return 0;
+}
