8fb3317ffad6cc06a5f641242ef9f52bba94f7ef
LoganMallory <logan.mallory@gmail.com>
2020-09-21 00:17:43 -0500

    standardized benchmark function so all timing takes place in one file

--- benchmark.cc
+++ benchmark.cc
@@ -1,159 +1,95 @@
-const int DEBUG = 0;
-int OPT_LVL;
+const int DEBUG      = 0;
+const int ITERATIONS = 100;
+const int MIN        = -1000000;
+const int MAX        =  1000000;
 #include "Vine.h"
-#include <cstdlib>
 #include <chrono>
-const int MIN = -10000000;
-const int MAX = 10000000;
-const unsigned int SIZE       = 500000;
-const unsigned int ITERATIONS = 1000;
-#include "benchmarks/constructor.cc"
-#include "benchmarks/destructor.cc"
-#include "benchmarks/assignment.cc"
-#include "benchmarks/indexing.cc"
-#include "benchmarks/equality.cc"
-#include "benchmarks/greaterthan.cc"
-#include "benchmarks/lessthan.cc"
-#include "benchmarks/addition.cc"
-#include "benchmarks/subtraction.cc"
-#include "benchmarks/multiplication.cc"
-#include "benchmarks/division.cc"
 
-
-void benchmark(double (*func)()) {
-  for(int i=0; i < 3; i++) {
-    OPT_LVL = i;
-    printf("\tOPT_LVL %d: %-8.2lf\n", OPT_LVL, func());
+void randomize_values(const Vine<int>& vec1, const Vine<int>& vec2, int& v) {
+  for(unsigned int i=0; i < vec1.length; i++) {
+    vec1.values[i] = MIN + (rand() % (MAX - MIN + 1));
+    vec2.values[i] = MIN + (rand() % (MAX - MIN + 1));
   }
+  v = MIN + (rand() % (MAX - MIN + 1));
+}
+
+Vine<int> add(const Vine<int>& vec1, const Vine<int>& vec2, int& v) {
+  return vec1 + v;
+}
+
+double benchmark(Vine<int> (*func)(const Vine<int>&, const Vine<int>&, int&), unsigned int size) {
+  //data
+  Vine<int> vec1(size);
+  Vine<int> vec2(size);
+  int v;
+
+  //measurements
+  double avg_duration = 0;
+  long n              = 0;
+  long elapsed;
+
+  for(int i=0; i < ITERATIONS; i++) {
+    printf("i: %d\n", i);
+    //randomize values
+    randomize_values(vec1, vec2, v);
+
+    //start timer
+    std::chrono::time_point< std::chrono::system_clock > start = std::chrono::system_clock::now();
+
+    //do work (avoid copying overhead by using RVO)
+    Vine<int> vec3 = func(vec1, vec2, v);
+
+    //end timer
+    std::chrono::time_point< std::chrono::system_clock > end = std::chrono::system_clock::now();
+    //calculate time elapsed
+    elapsed = (std::chrono::duration_cast<std::chrono::microseconds>(end - start)).count();
+    //update average
+    avg_duration = ((avg_duration * n) + elapsed) / (n+1);
+    n           += 1;
+
+    //write some data to disk so work isn't optimized out
+  }
+  return avg_duration;
+}
+
+double benchmark(void (*func)(const Vine<int>&, const Vine<int>&, int&), unsigned int size) {
+  //data
+  Vine<int> vec1(size);
+  Vine<int> vec2(size);
+  int v;
+
+  //measurements
+  double avg_duration = 0;
+  long n              = 0;
+  long elapsed;
+
+  for(int i=0; i < ITERATIONS; i++) {
+    printf("i: %d\n", i);
+    //randomize values
+    randomize_values(vec1, vec2, v);
+
+    //start timer
+    std::chrono::time_point< std::chrono::system_clock > start = std::chrono::system_clock::now();
+
+    //do work (avoid copying overhead by using RVO)
+    func(vec1, vec2, v);
+
+    //end timer
+    std::chrono::time_point< std::chrono::system_clock > end = std::chrono::system_clock::now();
+    //calculate time elapsed
+    elapsed = (std::chrono::duration_cast<std::chrono::microseconds>(end - start)).count();
+    //update average
+    avg_duration = ((avg_duration * n) + elapsed) / (n+1);
+    n           += 1;
+
+    //write some data to disk so work isn't optimized out
+  }
+  return avg_duration;
 }
 
 int main() {
-  srand(time(NULL));
+  unsigned int size = 500000; //5M
+  printf("microseconds: %lf\n", benchmark(add, size));
 
-  //constructor
-  printf("Vine()\n");
-  benchmark(benchmark_default_constructor);
-
-  printf("Vine(size)\n");
-  benchmark(benchmark_size_constructor);
-
-  printf("Vine(value, size)\n");
-  benchmark(benchmark_value_size_constructor);
-
-  printf("Vine(vine)\n");
-  benchmark(benchmark_vine_copy_constructor);
-
-  //destructor
-  printf("~Vine()\n");
-  benchmark(benchmark_destructor);
-
-  //assignment
-  printf("Vine = constant\n");
-  benchmark(benchmark_assignment_to_constant);
-
-  printf("Vine = Vine\n");
-  benchmark(benchmark_assignment_to_vine);
-
-  //indexing
-  printf("constant = Vine[i]\n");
-  benchmark(benchmark_index_get);
-
-  printf("Vine[i] = constant\n");
-  benchmark(benchmark_index_set);
-
-  //equality
-  printf("Vine == constant\n");
-  benchmark(benchmark_equal_to_constant);
-
-  printf("Vine == Vine\n");
-  benchmark(benchmark_equal_to_vine);
-
-  printf("Vine != constant\n");
-  benchmark(benchmark_not_equal_to_constant);
-
-  printf("Vine != Vine\n");
-  benchmark(benchmark_not_equal_to_vine);
-
-  //greater than
-  printf("Vine > constant\n");
-  benchmark(benchmark_greater_than_constant);
-
-  printf("Vine > Vine\n");
-  benchmark(benchmark_greater_than_vine);
-
-  printf("Vine >= constant\n");
-  benchmark(benchmark_greater_than_equal_to_constant);
-
-  printf("Vine >= Vine\n");
-  benchmark(benchmark_greater_than_equal_to_vine);
-
-  //less than
-  printf("Vine < constant\n");
-  benchmark(benchmark_less_than_constant);
-
-  printf("Vine < Vine\n");
-  benchmark(benchmark_less_than_vine);
-
-  printf("Vine <= constant\n");
-  benchmark(benchmark_less_than_equal_to_constant);
-
-  printf("Vine <= Vine\n");
-  benchmark(benchmark_less_than_equal_to_vine);
-
-  //addition
-  printf("Vine + constant\n");
-  benchmark(benchmark_add_constant);
-
-  printf("Vine + Vine\n");
-  benchmark(benchmark_add_vine);
-
-  printf("Vine += constant\n");
-  benchmark(benchmark_add_constant_inplace);
-
-  printf("Vine += Vine\n");
-  benchmark(benchmark_add_vine_inplace);
-
-  //subtraction
-  printf("Vine - constant\n");
-  benchmark(benchmark_subtract_constant);
-
-  printf("Vine - Vine\n");
-  benchmark(benchmark_subtract_vine);
-
-  printf("Vine -= constant\n");
-  benchmark(benchmark_subtract_constant_inplace);
-
-  printf("Vine -= Vine\n");
-  benchmark(benchmark_subtract_vine_inplace);
-
-  //multiplication
-  printf("Vine * constant\n");
-  benchmark(benchmark_multiply_constant);
-
-  printf("Vine * Vine\n");
-  benchmark(benchmark_multiply_vine);
-
-  printf("Vine *= constant\n");
-  benchmark(benchmark_multiply_constant_inplace);
-
-  printf("Vine *= Vine\n");
-  benchmark(benchmark_multiply_vine_inplace);
-
-  //division
-  printf("Vine / constant\n");
-  benchmark(benchmark_divide_constant);
-
-  printf("Vine / Vine\n");
-  benchmark(benchmark_divide_vine);
-
-  printf("Vine /= constant\n");
-  benchmark(benchmark_divide_constant_inplace);
-
-  printf("Vine /= Vine\n");
-  benchmark(benchmark_divide_vine_inplace);
   return 0;
 }
-
-//TODO: benchmarks that include `vec2 = vec1 + v;` or something similar are benchmarking the addition operator+ but also the assignent operator=, which differs based on OPTLVL
-//TODO: the above TODO may be void because of RVO
-//TODO: compiler is optimizing out some benchmarks because they have no side effects -- maybe write vec data to file?
